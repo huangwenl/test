9180b6c259647ef5be1847fa67187b987726268c
huangwenl <872222458@qq.com>
2016-10-18 15:57:28 +0800

    First

---
+++ src/test/java/HWL/COM/helloworld/AppTest1.java
@@ -0,0 +1,17 @@
+package HWL.COM.helloworld;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest1 {
+
+    @Test
+    public void testPrintHelloWorld() {
+	System.out.println("helloworld---ª∆Œƒ¡¡");
+	// Assert.assertEquals(App.getHelloWorld(), "Hello World");
+
+    }
+
+}
